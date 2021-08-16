9287888144bca680603a78c9e0d3c50e432b89af
coolmanben <76854955+coolmanben@users.noreply.github.com>
2021-08-17 00:01:38 +0300

    init: start task
    init: start task

--- src/index.js
+++ src/index.js
@@ -36,10 +36,39 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
+const expr = "0000101110000011111100000010110000111010**********00000000110000111111**********00101111100000101110000011111100001111100000101110000000101100000011110000001111000000101000000011100000111110";
+const result = "road to programming";
 
 function decode(expr) {
-    // write your solution here
+    let i = 0;
+    let res = '';
+    for ( let i = 1; i <= expr.length/10; i ++ ) {
+      let substring = expr.slice(i*10 - 10, i*10 );
+      let stringMotse = '';
+      if ( substring === '**********'){
+        stringMotse = ' ';  
+      }
+      else {
+        for ( let j = 10; j > 0; j = j - 2 ) {         
+          let substringMorse = substring.slice( j - 2, j ); 
+          switch (substringMorse) {
+            case '11':
+              stringMotse = '-' + stringMotse ;
+              break
+            case '10':
+              stringMotse = '.' + stringMotse ;
+              break
+            default:
+              break          
+          }                      
+        }        
+      stringMotse = MORSE_TABLE[stringMotse];
+      } 
+      res = res + stringMotse;
+    }
+    return res;
 }
+decode(expr);
 
 module.exports = {
     decode
